727fc605746c269b262c4f3e23a016b6842f7f71
Lucas Carvalho Correia <lucascarvalhocorreia>
2021-11-04 17:16:47 -0300

    operadores e esteruturas condicionais

---
+++ estrutura_condicional.rb
@@ -0,0 +1,25 @@
+puts 'digite um numero 1 ou 2'
+v1 = gets.to_1
+
+# if v1 == 1
+#     puts 'valor igual a 1'
+# elsif v1 == 2
+#     puts 'valor igual e 2'
+# else
+#     puts 'valor nao e igual a 1
+# end
+
+unless v1 == 2
+    puts 'condicao falsa'
+else
+    puts 'condicao verdadeira'
+end
+
+case v1
+when 0
+    puts 'digitou 0'
+when 1
+    puts 'digiout 1'
+else
+    puts 'opcao invalida'
+end
